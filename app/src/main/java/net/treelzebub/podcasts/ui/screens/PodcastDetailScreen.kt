d1501547e2049f9a15a9c28ec6525f6268bc9c47
treelzebub <treelzebub@users.noreply.github.com>
2024-02-12 12:05:05 -0800

    cleanup

--- app/src/main/java/net/treelzebub/podcasts/ui/screens/PodcastDetailScreen.kt
+++ app/src/main/java/net/treelzebub/podcasts/ui/screens/PodcastDetailScreen.kt
@@ -11,8 +11,8 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
