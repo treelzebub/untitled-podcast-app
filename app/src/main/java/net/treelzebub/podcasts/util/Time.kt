0b064f389abf5bad4025f9a76f0606367043b0da
treelzebub <treelzebub@users.noreply.github.com>
2024-02-14 10:15:30 -0800

    fix timezone bug

--- app/src/main/java/net/treelzebub/podcasts/util/Time.kt
+++ app/src/main/java/net/treelzebub/podcasts/util/Time.kt
@@ -1,14 +1,14 @@
 package net.treelzebub.podcasts.util
 
 import java.time.LocalDateTime
-import java.time.ZoneOffset
+import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
-import java.util.TimeZone
 
 object Time {
     private val formatter = DateTimeFormatter.RFC_1123_DATE_TIME
     private val displayFormat = DateTimeFormatter.ofPattern("MMMM d, yyyy")
 
-    fun nowTimestamp(): String = LocalDateTime.now().atOffset(ZoneOffset.UTC).format(formatter)
-    fun displayFormat(string: String?): String = string?.let { LocalDateTime.parse(string, formatter).format(displayFormat) }.orEmpty()
+    fun nowTimestamp(): String = ZonedDateTime.now().format(formatter)
+    fun displayFormat(string: String?): String =
+        string?.let { LocalDateTime.parse(string, formatter).format(displayFormat) }.orEmpty()
 }
