81a637e49066e2bf0405737201716f46e4d7b4f9
treelzebub <treelzebub@users.noreply.github.com>
2024-04-22 17:29:31 -0700

    root:build.gradle -> version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    kotlin("kapt") version "1.9.23"
-    id("com.android.application") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-    id("com.google.devtools.ksp") version "1.9.23-1.0.20" apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin) apply false
+    alias(libs.plugins.kapt) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt) apply false
 }
