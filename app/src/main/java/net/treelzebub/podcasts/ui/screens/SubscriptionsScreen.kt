7d00674138f101c06983ddfda61fd87fd4b6ef47
treelzebub <treelzebub@users.noreply.github.com>
2024-07-21 15:55:16 -0700

    remove lazycolumn version of main screen

--- app/src/main/java/net/treelzebub/podcasts/ui/screens/SubscriptionsScreen.kt
+++ app/src/main/java/net/treelzebub/podcasts/ui/screens/SubscriptionsScreen.kt
@@ -1,124 +1,64 @@
 package net.treelzebub.podcasts.ui.screens
 
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.VisibilityThreshold
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.text.BasicText
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.AsyncImage
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import net.treelzebub.podcasts.ui.components.LoadingBox
-import net.treelzebub.podcasts.ui.models.PodcastUi
 import net.treelzebub.podcasts.ui.screens.destinations.PodcastDetailsScreenDestination
-import net.treelzebub.podcasts.ui.theme.LightestPurple
-import net.treelzebub.podcasts.ui.theme.TextStyles
 import net.treelzebub.podcasts.ui.vm.SubscriptionsViewModel
 
-
-//@RootNavGraph(start = true)
+@RootNavGraph(start = true)
 @Destination
 @Composable
 fun SubscriptionsScreen(navigator: DestinationsNavigator) {
     val vm = hiltViewModel<SubscriptionsViewModel>()
-    val state by remember { vm.state }.collectAsState()
+    val state by remember { vm.state }.collectAsStateWithLifecycle()
 
     if (state.loading) {
         LoadingBox()
     } else {
-        Scaffold() { contentPadding ->
-            PodcastList(navigator, state.podcasts, contentPadding)
-        }
-    }
-}
-
-@Composable
-@OptIn(ExperimentalFoundationApi::class)
-fun PodcastList(
-    navigator: DestinationsNavigator,
-    podcasts: List<PodcastUi>,
-    contentPadding: PaddingValues
-) {
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 8.dp)
-    ) {
-        items(podcasts, key = { it.id }) {
-            PodcastItem(Modifier.animateItemPlacement(tween(durationMillis = 250)), navigator, it, contentPadding)
-        }
-    }
-}
-
-@Composable
-fun PodcastItem(
-    modifier: Modifier,
-    navigator: DestinationsNavigator,
-    podcast: PodcastUi,
-    contentPadding: PaddingValues
-) {
-    Row(
-        modifier = Modifier
-            .wrapContentHeight()
-            .fillMaxWidth()
-            .padding(contentPadding)
-            .clickable { navigator.navigate(PodcastDetailsScreenDestination(podcast.id)) }
-            .then(modifier),
-        verticalAlignment = Alignment.Top
-    ) {
-        AsyncImage(
-            modifier = Modifier
-                .padding(top = 16.dp)
-                .fillMaxSize()
-                .weight(1.0f),
-            model = podcast.imageUrl,
-            contentDescription = "Podcast Logo"
-        )
-        Column(
-            Modifier
-                .weight(3f)
-                .wrapContentHeight()
-                .padding(12.dp)
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(12.dp),
+            horizontalArrangement = Arrangement.Center
         ) {
-            Text(
-                modifier = Modifier.padding(bottom = 2.dp),
-                style = TextStyles.CardSubtitle,
-                text = podcast.title
-            )
-            Text(
-                modifier = Modifier.padding(bottom = 2.dp),
-                style = TextStyles.CardDescription,
-                text = podcast.description,
-            )
-            BasicText(
-                style = TextStyles.CardDate,
-                overflow = TextOverflow.Ellipsis,
-                text = "Updated: " + podcast.lastBuildDate
-            )
+            items(state.podcasts) {
+                AsyncImage(
+                    modifier = Modifier.animateItem(
+                        placementSpec = spring(
+                            stiffness = Spring.StiffnessMediumLow,
+                            visibilityThreshold = IntOffset.VisibilityThreshold)
+                        )
+                        .fillMaxSize()
+                        .padding(4.dp)
+                        .shadow(elevation = 4.dp)
+                        .clickable { navigator.navigate(PodcastDetailsScreenDestination(it.id)) },
+                    model = it.imageUrl,
+                    contentDescription = "Artwork for podcast ${it.title}"
+                )
+            }
         }
     }
-    HorizontalDivider(
-        thickness = 0.5.dp,
-        color = LightestPurple
-    )
 }
