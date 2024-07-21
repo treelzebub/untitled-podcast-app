e418997bd762902ca56e9a309fa507c7218f97e1
treelzebub <treelzebub@users.noreply.github.com>
2024-07-20 21:12:33 -0700

    fix episode list animation; pin header to the top of list

--- app/src/main/java/net/treelzebub/podcasts/ui/screens/PodcastDetailScreen.kt
+++ app/src/main/java/net/treelzebub/podcasts/ui/screens/PodcastDetailScreen.kt
@@ -1,5 +1,8 @@
 package net.treelzebub.podcasts.ui.screens
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.VisibilityThreshold
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -7,9 +10,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyItemScope
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
@@ -28,15 +35,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.AsyncImage
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
-import net.treelzebub.podcasts.ui.components.EpisodesList
+import net.treelzebub.podcasts.ui.components.ItemCard
 import net.treelzebub.podcasts.ui.components.LoadingBox
+import net.treelzebub.podcasts.ui.models.EpisodeUi
 import net.treelzebub.podcasts.ui.models.PodcastUi
+import net.treelzebub.podcasts.ui.screens.destinations.EpisodeDetailDestination
 import net.treelzebub.podcasts.ui.theme.TextStyles
 import net.treelzebub.podcasts.ui.vm.PodcastDetailsViewModel
 import timber.log.Timber
@@ -80,18 +90,20 @@ private fun PodcastDetails(
     onToggleShowPlayed: () -> Unit,
     onDelete: () -> Unit
 ) {
-    Column(Modifier.fillMaxSize()) {
-        PodcastHeader(
-            podcast = state.podcast!!,
-            showPlayed = state.showPlayed,
-            onToggleShowPlayed = onToggleShowPlayed,
-            onDelete = onDelete
-        )
-        EpisodesList(
-            navigator = navigator,
-            modifier = Modifier.weight(4f),
-            episodes = state.episodes
-        )
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        item {
+            PodcastHeader(
+                podcast = state.podcast!!,
+                showPlayed = state.showPlayed,
+                onToggleShowPlayed = onToggleShowPlayed,
+                onDelete = onDelete
+            )
+        }
+        items(items = state.episodes, key = { it.id }) {
+            EpisodeItem(navigator, it)
+        }
     }
 }
 
@@ -153,8 +165,47 @@ private fun PodcastHeader(
                     }
                 )
                 DropdownMenuItem(text = { Text(text = "Delete") }, onClick = onDelete)
-
             }
+        }
+    }
+}
+
+@Composable
+// Extension on LazyItemScope makes importing animateItem() possible.
+private fun LazyItemScope.EpisodeItem(navigator: DestinationsNavigator, episode: EpisodeUi) {
+    ItemCard(
+        modifier = Modifier.animateItem(
+            placementSpec = spring(
+                stiffness = Spring.StiffnessMediumLow,
+                visibilityThreshold = IntOffset.VisibilityThreshold
+            )
+        ).clickable {
+            navigator.navigate(EpisodeDetailDestination(episode.id))
+        }
+    ) {
+        Column(
+            Modifier
+                .weight(3.0f)
+                .wrapContentHeight()
+                .fillMaxWidth()
+                .padding(start = 12.dp, top = 6.dp, end = 12.dp, bottom = 6.dp)
+        ) {
+            BasicText(
+                modifier = Modifier.padding(bottom = 2.dp),
+                style = TextStyles.CardSubtitle,
+                text = episode.title
+            )
+            BasicText(
+                modifier = Modifier.padding(bottom = 2.dp),
+                style = TextStyles.CardDate,
+                text = episode.displayDate
+            )
+            BasicText(
+                modifier = Modifier.height(72.dp),
+                style = TextStyle(textAlign = TextAlign.Start),
+                overflow = TextOverflow.Ellipsis,
+                text = episode.description
+            )
         }
     }
 }
