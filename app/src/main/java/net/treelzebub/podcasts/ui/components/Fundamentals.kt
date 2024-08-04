520f423fc51894e0f3afd961e8422d490ce2e635
tre murillo <treelzebub@users.noreply.github.com>
2024-08-03 20:16:15 -0700

    progress checkin (#47)

--- app/src/main/java/net/treelzebub/podcasts/ui/components/Fundamentals.kt
+++ app/src/main/java/net/treelzebub/podcasts/ui/components/Fundamentals.kt
@@ -1,14 +1,25 @@
 package net.treelzebub.podcasts.ui.components
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import net.treelzebub.podcasts.R
+import net.treelzebub.podcasts.ui.vm.EpisodeDetailsViewModel.Action.Download
 
 
 @Composable
@@ -25,4 +36,30 @@ fun ItemCard(
         shape = RoundedCornerShape(6.dp),
         onClick = onClick
     ) { content() }
+}
+
+@Composable
+fun ButtonCircleBorderless(
+    modifier: Modifier = Modifier,
+    @DrawableRes res: Int,
+    contentDescription: String,
+    tint: Color = Color.Black,
+    onClick: () -> Unit = {}
+) {
+    OutlinedButton(
+        modifier = Modifier
+            .size(48.dp)
+            .then(modifier),
+        shape = CircleShape,
+        border = BorderStroke(0.dp, Color.White),
+        contentPadding = PaddingValues(0.dp),
+        onClick = onClick
+
+    ) {
+        Icon(
+            painter = painterResource(id = res),
+            tint = tint,
+            contentDescription = contentDescription
+        )
+    }
 }
