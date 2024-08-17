bb48672ba283adfe8b6bd624ad6e6ce8f45f2eb9
tre murillo <treelzebub@users.noreply.github.com>
2024-08-17 11:58:45 -0700

    dep bumps (#50)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    /**
+     * Remove once Dagger and Hilt support for KSP is stable
+     *   https://kotlinlang.org/docs/ksp-overview.html#supported-libraries
+     */
     alias(libs.plugins.kapt) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
